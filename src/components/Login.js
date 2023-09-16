c04a466237ee9ac885961b6c10d1119b8ae77d7a
sahilkumar0001 <sahil.sk6862@gmail.com>
2023-09-16 20:02:34 +0530

    modified:   src/components/Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Login() {
   const navigate = useNavigate();
   const [user, setUser] = useState({ email: "", password: "" });
-  const [error, seterror] = useState(false)
+  const [error, seterror] = useState(false);
   const onchange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
@@ -16,70 +16,80 @@ function Login() {
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify({ email, password })
+      body: JSON.stringify({ email, password }),
     });
-    if ( response.status === 200) {
-      const id =  await response.json();
-      localStorage.setItem("auth",id._id);
-      navigate("/")
-    }
-    else{
+    if (response.status === 200) {
+      const id = await response.json();
+      localStorage.setItem("auth", id._id);
+      navigate("/");
+    } else {
       seterror(true);
     }
   };
   return (
     <div className="container">
-      <div className={`alert alert-danger alert-dismissible fade show align-left ${(error)?"":"d-none"}`} role="alert" style={{    width: "40%",
-    position: "absolute",
-    top: "55px",
-    left: "0"}}>
-  <strong>Enter valid credentials!</strong>
-  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
+      <div
+        className={`alert alert-danger alert-dismissible fade show align-left ${
+          error ? "" : "d-none"
+        }`}
+        role="alert"
+        style={{ width: "40%", position: "absolute", top: "55px", left: "0" }}
+      >
+        <strong>Enter valid credentials!</strong>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
       <h2 className="text-center">Login</h2>
       <div
         className="login container  m-auto w-70 p-5"
         style={{ width: "55%", minWidth: "500px" }}
       >
-       
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              name="email"
-              onChange={onchange}
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            name="email"
+            onChange={onchange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              onChange={onchange}
-            />
-          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={onchange}
+          />
+        </div>
 
-          <button type="submit" className="btn btn-primary" onClick={login}>
-            Submit
-          </button>
-        
+        <button type="submit" className="btn btn-primary" onClick={login}>
+          Submit
+        </button>
       </div>
-      <h5 className="text-center">Don't have an account?<Link to="/register" className="link-success" >Register</Link></h5>
+      <h5 className="text-center">
+        Don't have an account?
+        <Link to="/register" className="link-success">
+          Register
+        </Link>
+      </h5>
       <div id="emailHelp" className="form-text text-center pt-3">
-             Demo account: email=demo@gmail.com, password=demo
-            </div>
+        Demo account: email=demo@gmail.com, password=demo
+      </div>
     </div>
   );
 }
